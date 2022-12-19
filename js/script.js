fbf458f91278251666b590263f2bea2432756643
Iwona Gasidlo <iwona.gasidlo2810@gmail.com>
2022-12-19 16:33:22 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+console.log("Cześć!");
+
+let button = document.querySelector(".js-button");
+let body = document.querySelector(".body");
+let themeName = document.querySelector(".themeName");
+
+button.addEventListener("click", () => {
+    body.classList.toggle("dark");
+
+    themeName.innerText =
+        body.classList.contains("dark") ?
+            "jasny" : "ciemny";
+})
